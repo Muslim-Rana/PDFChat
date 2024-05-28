0860a14decbef87d19366e3d1e6ba2f0de7a63a8
Muslim-Rana <muslim.inaam.rana@gmail.com>
2024-05-28 14:32:15 -0400

    Completed the model using open source LLM from hugging face

--- app.py
+++ app.py
@@ -32,11 +32,20 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":1024, "max_new_tokens": 70})
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm = llm, retriever=vectorstore.as_retriever(), memory = memory)
     return conversation_chain
-     
+
+def handle_userinput(user_question):
+     response = st.session_state.conversation({'question': user_question})
+     st.session_state.chat_history = response['chat_history']
+
+     for i, message in enumerate(st.session_state.chat_history):
+          if i % 2 == 0:
+               st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+          else:
+               st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 def main():
     load_dotenv()
@@ -46,13 +55,13 @@ def main():
 
     if "conversation" not in st.session_state:
          st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+         st.session_state.chat_history = None
 
-    st.header("Chat with your own PDFs :books:")
-    st.text_input("Ask anything about your PDFs:")
-    
-    st.write(user_template.replace("{{MSG}}", "Hi"), unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "Hello"), unsafe_allow_html=True)
-
+    st.header("ChatPDF :books:")
+    user_question = st.text_input("Ask anything about your PDFs:")
+    if user_question:
+         handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
